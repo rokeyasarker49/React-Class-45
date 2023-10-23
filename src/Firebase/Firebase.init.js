2995c653b6c976b57b9cb845c0191f14e394a611
Rokeya Sarker <133252712+rokeyasarker49@users.noreply.github.com>
2023-10-23 16:27:29 +0600

    Task Completed

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAPrIwup3D2NfGHbcM6jFV6894DeMCilpc",
-  authDomain: "learn-react-09.firebaseapp.com",
-  projectId: "learn-react-09",
-  storageBucket: "learn-react-09.appspot.com",
+  authDomain: "react-class-45.firebaseapp.com",
+  projectId: "react-class-45",
+  storageBucket: "react-class-45.appspot.com",
   messagingSenderId: "627193049113",
   appId: "1:627193049113:web:6ceb225c0001e74cf57a88"
 };
